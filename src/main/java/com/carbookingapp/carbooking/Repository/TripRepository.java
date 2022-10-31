726424d7dccb945e939c797f3f631c4eae69993d
Jeevananthan-23 <jeeva023@outlook.com>
2022-10-31 19:42:17 +0530

    hashtableconcurrency

--- src/main/java/com/carbookingapp/carbooking/Repository/TripRepository.java
+++ src/main/java/com/carbookingapp/carbooking/Repository/TripRepository.java
@@ -7,29 +7,33 @@ import com.carbookingapp.carbooking.Models.Driver;
 import com.carbookingapp.carbooking.Models.Location;
 
 public class TripRepository{
-  private static Map<String, Driver> rider= new HashMap<>();
-  private static int userSearchRadius=5;
- static List<Driver> driverList = DriverRepository.getDrivers();
-  public Map<String, Driver> getRider() {
+ static Map<Driver, String> rider= new HashMap<>();
+  private int userSearchRadius=5;
+  private List<Driver> drivers= DriverRepository.getDrivers();
+  public static Map<Driver, String> getRider() {
     return rider;
     }  
-    public static synchronized List<Driver> findRide(String name, Location source, Location destination)throws NoRideFound{
+    public synchronized List<Driver> findRide(String name, Location source, Location destination)throws NoRideFound{
       List<Driver> result = new ArrayList<>();
-      for (Driver cab : driverList) {
-        if (cab.getCurrentlocation().distance(source) <=userSearchRadius && !rider.containsValue(cab)) {
+      for (Driver cab : drivers) {
+        if (cab.getCurrentlocation().distance(source) <=userSearchRadius && !rider.containsKey(cab)) {
           result.add(cab);
-          rider.put(name, cab);
+          rider.put(cab,name);
           System.out.println(name+" booked "+cab.getName());
         }
       }
       try {
-        if(!rider.containsKey(name)){
+        if(!rider.containsValue(name)){
           throw new NoRideFound("No Ride found");
         }
       } catch (NoRideFound e) {
        System.out.println(e.getMessage());
       }
-      
       return result;
       }
+      public String chooseRider(String username, Driver drivername){
+        if(getRider().containsValue(username)&& getRider().containsKey(drivername))
+        System.out.println(username+" booked "+drivername);
+     return null;
+         }
 }
--- src/main/java/com/carbookingapp/carbooking/Service/TripService.java
+++ src/main/java/com/carbookingapp/carbooking/Service/TripService.java
@@ -1,8 +1,6 @@
 package com.carbookingapp.carbooking.Service;
 
 
-import java.util.List;
-
 import com.carbookingapp.carbooking.Models.Driver;
 import com.carbookingapp.carbooking.Models.Location;
 import com.carbookingapp.carbooking.Repository.TripRepository;
@@ -11,8 +9,12 @@ public class TripService extends Thread {
   private String name;
   private Location source;
   private Location destination;
-  private TripRepository tripRepo;
+  private static TripRepository tripRepo;
 
+  @Override
+  public void run(){
+    tripRepo.findRide(name, source, destination);
+  }
   public TripService(String name, Location source, Location destination) {
     this.name=name;
     this.source=source;
@@ -23,16 +25,8 @@ public class TripService extends Thread {
   public TripService() {
       tripRepo = new TripRepository();
   }
-  @Override
-  public void run(){
-    findRide(name, source, destination);
-  }
+
     public String chooseRider(String username, Driver drivername){
-        if(tripRepo.getRider().containsKey(username)&& tripRepo.getRider().containsValue(drivername))
-          System.out.println(drivername);
-       return null;
+      return tripRepo.chooseRider(username, drivername);
        }
-     public synchronized List<Driver> findRide(String name, Location source, Location destination){
-      return TripRepository.findRide(name, source, destination);
-     }
 }
